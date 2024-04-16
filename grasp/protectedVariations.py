cb6200e0a48f2780888bf3dbfa04a29f116db647
SenaVitor <vitorlucasdesenalima@gmail.com>
2024-04-15 23:10:05 -0300

    update protectedVariations example

--- grasp/protectedVariations.py
+++ grasp/protectedVariations.py
@@ -10,30 +10,40 @@ class Vehicle(ABC):
 # Implementation of the first variation of the service
 # Implementação da primeira variação do serviço
 class Car(Vehicle):
+    def __init__(self, color):
+        self.color = color
+        self.wheels = 4
+
     def drive(self):
         print("Driving car")
 
 # Implementation of the second variation of the service
 # Implementação da segunda variação do serviço
 class Motorcycle(Vehicle):
+    def __init__(self, color):
+        self.color = color
+        self.wheels = 2
+
     def drive(self):
         print("Driving motorcycle")
 
 # Cliente interagindo com a interface estável
 class Driver:
-    def __init__(self, service: Vehicle):
-        self.service = service
+    def __init__(self, name, vehicle: Vehicle, age):
+        self.name = name
+        self.vehicle = vehicle
+        self.age = age
     
     def driveVehicle(self):
-        self.service.drive()
+        self.vehicle.drive()
 
 # Exemplo de uso
 if __name__ == "__main__":
-    vehicle_a = Car()
-    vehicle_b = Motorcycle()
+    vehicle_a = Car("red")
+    vehicle_b = Motorcycle("yellow")
 
-    driver_a = Driver(vehicle_a)
+    driver_a = Driver("Vitor", vehicle_a, 21)
     driver_a.driveVehicle()  # Output: Driving car
 
-    driver_b = Driver(vehicle_b)
+    driver_b = Driver("Lucas", vehicle_b, 22)
     driver_b.driveVehicle()  # Output: Driving motorcycle
